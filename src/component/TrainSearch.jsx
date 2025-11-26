e6144c0e0454ce624155adb4ad7b6e61cb8b8491
sathishramachandran <sathishramu1999@gmail.com>
2025-11-26 14:57:52 +0530

    add train compo

--- src/component/TrainSearch.jsx
+++ src/component/TrainSearch.jsx
@@ -1,19 +1,20 @@
-import React from 'react'
-import styled from 'styled-components'
+import React from "react";
+import styled from "styled-components";
 
-const Container = styled.div `
-background-color : white;
-padding: 1rem;
-border-radius: 5px;
-box-shadow: 0px 4px 8px rgba(0,0,0,)
-`
+const Container = styled.div`
+  background-color: white;
+  padding: 1rem;
+  border-radius: 5px;
+  box-shadow: 0px 4px 8px rgba(0, 0, 0, 0.2);
+  text-align: center;
+`;
 
 export default function TrainSearch() {
   return (
-<>
-<div>
-train search
-</div>
-</>
-  )
+    <>
+      <Container>
+        <h2>Search for trains</h2>
+      </Container>
+    </>
+  );
 }
